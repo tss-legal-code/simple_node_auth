71c38631be8d348b2dac9e6b1c6c12f8cb20252d
tss-legal-code <tss.legal.code@gmail.com>
2022-12-22 14:05:26 +0200

    added user registration

--- authController.js
+++ authController.js
@@ -1,11 +1,23 @@
 const User = require('./models/User');
 const Role = require('./models/Role');
+const bcrypt = require('bcryptjs');
 class authController {
   async registartion(req, res) {
     try {
-
+      const { username, password } = req.body;
+      const candidate = await User.findOne({ username });
+      if (candidate) {
+        return res.status(400).json({ message: "User already registered" });
+      }
+      const passwordSalt = bcrypt.genSaltSync(7);
+      const passwordHash = bcrypt.hashSync(password, passwordSalt);
+      const userRole = await Role.findOne({ value: "USER" });
+      const user = new User({ username, password: passwordHash, roles: [userRole.value] });
+      await user.save();
+      return res.json({ message: "User is now sccessfully registered" });
     } catch (error) {
       console.log(error);
+      res.status(400).json({ message: "Registration error" });
     }
   };
   async login(req, res) {
@@ -13,6 +25,7 @@ class authController {
 
     } catch (error) {
       console.log(error);
+      res.status(400).json({ message: "Login error" });
     }
   };
   async getUsers(req, res) {
@@ -27,6 +40,7 @@ class authController {
       res.json('server works');
     } catch (error) {
       console.log(error);
+      res.status(400).json({ message: "Access users error" });
     }
   };
 }
